526466708cafeabb91a1abf1f41abbf712654505
lutfiayven <149711359+lutfiayven@users.noreply.github.com>
2023-11-09 10:46:06 -0600

    initial commit
    added background and data

---
+++ indexScripts.js
@@ -0,0 +1,11 @@
+document.getElementById('cancelBtn').addEventListener("click", function(){
+    window.close();
+})
+
+document.getElementById('prev1Btn').addEventListener("click", function(){
+    location.href = "index.html";
+})
+
+document.getElementById('next1Btn').addEventListener("click", function(){
+    location.href = "page2.html";
+})
